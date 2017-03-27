a6a3fc82c40606479a800412ff75cf6074e6a318
Oliver Pelz <oliverpelz@gmail.com>
2017-03-27 16:22:54 +0200

    working on mapper, included argparser

--- sam_mapper_in_RUST/sam_mapper/src/main.rs
+++ sam_mapper_in_RUST/sam_mapper/src/main.rs
@@ -1,22 +1,50 @@
 extern crate regex;
+extern crate argparse;
+
 
 use regex::Regex;
 use std::env;
 use std::process;
 use std::fs::File;
+use argparse::{ArgumentParser, Store};
 
 use std::io::{BufReader, BufRead, BufWriter, Write};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        println!("error, argument missing: a sam file");
-        process::exit(1);
-    }
-    let sam_file_arg = &args[1];
+    let mut fasta_file_arg   = String::new();
+    let mut sam_file_arg = String::new();
+    let mut mapping_match_pattern = String::from("M{20,21}$");
+    let mut geneid_pattern = String::from("_");
+    let mut logfile_out    = String::from("./log.out");
+{
+    let mut cli_parser = ArgumentParser::new();
+    cli_parser.set_description("mapper for CRISPRanalyzer");
+    
+    cli_parser.refer(&mut fasta_file_arg)
+            .add_option(&["-f", "--fasta-file"], Store,
+            "Fasta Library Input File").required();
+    
+    cli_parser.refer(&mut sam_file_arg)
+            .add_option(&["-s", "--sam-file"], Store,
+            "Sam Input file").required();
+    
+    cli_parser.refer(&mut mapping_match_pattern)
+            .add_option(&["-m", "--match-pattern"], Store,
+            "Mapping match pattern e.g. M{20,21}$");
+
+    cli_parser.refer(&mut geneid_pattern)
+            .add_option(&["-g", "--geneid-pattern"], Store,
+            "GeneId pattern to parse, e.g. '_'");
+
+    cli_parser.refer(&mut logfile_out)
+            .add_option(&["-l", "--logfile"], Store,
+            "Logfile filename");
+
+    cli_parser.parse_args_or_exit();
+}
     let mut next_line = String::new();
-    let mut sam_file = BufReader::new(File::open(sam_file_arg).expect("Problem opening fastq file"));
+    let mut sam_file = BufReader::new(File::open(&mut sam_file_arg).expect("Problem opening fastq file"));
    
 // fast forward the sam header to the beginning of the
 // alignment section
@@ -44,7 +72,7 @@ fn main() {
 	let mut al_arr: Vec<&str> = alignment.trim_right().split("\t").collect();
         
 	let mut gene_id = al_arr[2].split("_").nth(0).unwrap();	
-
+	
 
 
 
