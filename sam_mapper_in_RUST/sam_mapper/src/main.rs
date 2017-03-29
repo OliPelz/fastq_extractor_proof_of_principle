315794c5d3254ff29cf3cae37b341efd573955b7
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-03-29 16:25:57 +0200

    compare against oli/master

--- sam_mapper_in_RUST/sam_mapper/src/main.rs
+++ sam_mapper_in_RUST/sam_mapper/src/main.rs
@@ -24,23 +24,23 @@ fn main() {
         cli_parser.set_description("mapper for CRISPRanalyzer");
 
         cli_parser.refer(&mut fasta_file_arg)
-            .add_option(&["-f", "--fasta-file"], Store, "Fasta Library Input File")
-            .required();
+            .add_option(&["-f", "--fasta-file"], Store,
+            "Fasta Library Input File").required();
 
         cli_parser.refer(&mut sam_file_arg)
-            .add_option(&["-s", "--sam-file"], Store, "Sam Input file")
-            .required();
+            .add_option(&["-s", "--sam-file"], Store,
+            "Sam Input file").required();
 
         cli_parser.refer(&mut mapping_match_pattern)
             .add_option(&["-m", "--match-pattern"], Store,
             "Mapping match pattern e.g. M{20,21}$");
 
-        cli_parser.refer(&mut geneid_pattern).add_option(&["-g", "--geneid-pattern"],
-                                                         Store,
+        cli_parser.refer(&mut geneid_pattern)
+            .add_option(&["-g", "--geneid-pattern"], Store,
                                                          "GeneId pattern to parse, e.g. '_'");
 
-        cli_parser.refer(&mut logfile_out).add_option(&["-l", "--logfile"],
-                                                      Store,
+        cli_parser.refer(&mut logfile_out)
+            .add_option(&["-l", "--logfile"], Store,
                                                       "Logfile filename");
 
         cli_parser.parse_args_or_exit();
@@ -51,35 +51,32 @@ fn main() {
     let mut geneids = HashSet::<String>::new();
 
     let fasta_re = Regex::new(format!("{}{}", String::from("^>(.+)\n$"), geneid_pattern).as_str())
-        .expect("programmer error in accession regex");
-    let fasta_file =
-        BufReader::new(File::open(fasta_file_arg).expect("Problem opening fastq file"));
+            .expect("programmer error in accession regex");
+    let fasta_file = BufReader::new(File::open(fasta_file_arg).expect("Problem opening fastq file"));
 
-    {
-        for line in fasta_file.lines() {
-            let ln = line.expect("programmer error in reading fasta line by line");
 
-            let caps = fasta_re.captures(&ln).unwrap();
-            if let Some(first_cap) = caps.get(1) {
-                geneids.insert(String::from(first_cap.as_str()));
-            }
+    for line in fasta_file.lines() {
+        let ln = line.expect("programmer error in reading fasta line by line");
+
+        let caps = fasta_re.captures(&ln).unwrap();
+        if let Some(first_cap) = caps.get(1) {
+            geneids.insert(String::from(first_cap.as_str()));
         }
     }
-    println!("{}", geneids.len());
+    //println!("{}", geneids.len());
 
 
-    // our buffer
+// now to the sam parser
+// TODO: implement as XVS stream parser
+// our buffer for the sam parser
     let mut next_line = String::new();
-
-    let mut sam_file =
-        BufReader::new(File::open(&mut sam_file_arg).expect("Problem opening fastq file"));
+    let mut sam_file = BufReader::new(File::open(&mut sam_file_arg).expect("Problem opening fastq file"));
 
     // fast forward the sam header to the beginning of the
-    // alignment section
+// alignment section - skip the header starting with @
     let mut alignment = String::from("");
 
     while sam_file.read_line(&mut next_line).unwrap() > 0 {
-        //println!("{}" , next_line);
         if !next_line.starts_with('@') {
             break;
         }
@@ -91,17 +88,33 @@ fn main() {
 
     if !next_line.is_empty() {
         // thats how to construct a do { } while loop in RUST
+       let mut mismatches_allowed = false;
+
+       if mapping_match_pattern.contains('x') || mapping_match_pattern.contains('X') {
+           mismatches_allowed = true;
+       }
+
+       //let sam_mismatch_re = Regex::new(r"MD:Z:([0-9+]|[A-Z]+)+\s.*$" ).expect("programmer error in accession regex");
+       let sam_mismatch_re = Regex::new(r"(MD:Z:[0-9A-Z]+).*").expect("programmer error in accession regex");
+
         loop {
             count_total += 1;
             // ----------the basic algorithm starts here ---
             alignment = next_line.clone();
-            //print!("{}", alignment);
             // now split
             let mut al_arr: Vec<&str> = alignment.trim_right().split("\t").collect();
-
             let mut gene_id = al_arr[2].split("_").nth(0).unwrap();
-
-
+        // the sam file format is so BAD that a certain position of any optional field cannot be
+        // predicted for sure, so we need to parse the whole line for the mismatch string
+        // at least we know that we have to search from the right end to the left because in the
+        // beginning we have mandantory fields (first 11)
+           let mut found_mismatches = false;
+           //println!("{}", alignment);
+           for caps in sam_mismatch_re.captures_iter(&alignment) {
+              //geneids.insert(String::from(&caps[1]));
+              found_mismatches = true;
+              println!("BINGO {}", &caps[1]);
+           }
 
 
             // --------- end of basic algorithm ---
@@ -117,6 +130,4 @@ fn main() {
 
     println!("Total\tMatched");
     println!("{}\t{}", count_total, count_total);
-
-
 }
