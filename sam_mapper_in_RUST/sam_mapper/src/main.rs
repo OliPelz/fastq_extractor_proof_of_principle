44e99832852e2dd3980fa2412cfe24d8d57ec204
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-03-30 17:52:10 +0200

    move comment back to where it belongs...
    still trying to figure out how to use .map() or .filter() here...
    We shouldn't NEED a do-while loop at all

--- sam_mapper_in_RUST/sam_mapper/src/main.rs
+++ sam_mapper_in_RUST/sam_mapper/src/main.rs
@@ -84,11 +84,12 @@ fn main() {
     let mut count_total = 0;
 
     if !next_line.is_empty() {
-        // thats how to construct a do { } while loop in RUST
         let mismatch_in_patt = mapping_match_pattern.contains('x') || mapping_match_pattern.contains('X');
 
         let sam_mismatch_re = Regex::new(r"MD:Z:([0-9]+)([A-Z]+)([0-9])+" ).expect("programmer error in mismatch regex");
         let match_string_re = Regex::new(r"(([0-9]+)([MID]))+").expect("programmer error in match regex");
+
+        // thats how to construct a do { } while loop in RUST
         loop {
             count_total += 1;
             // ----------the basic algorithm starts here ---
