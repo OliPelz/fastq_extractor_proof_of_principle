b5cc122766d4b0274ac57d13b0b104bf8ff076f8
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-04-06 11:08:38 +0200

    improve formatting

--- sam_mapper_in_RUST/sam_mapper/src/main.rs
+++ sam_mapper_in_RUST/sam_mapper/src/main.rs
@@ -18,18 +18,24 @@ fn main() {
     let mut geneid_pattern = String::from("_");
     let mut logfile_out = String::from("./log.out");
 
-    parse_args(&mut fasta_file_arg, &mut sam_file_arg, &mut mapping_match_pattern, &mut geneid_pattern, &mut logfile_out);
+    parse_args(&mut fasta_file_arg,
+               &mut sam_file_arg,
+               &mut mapping_match_pattern,
+               &mut geneid_pattern,
+               &mut logfile_out);
 
     let fasta_re = Regex::new(&format!(r"^>(.+){}", geneid_pattern))
             .expect("programmer error in accession regex");
 
-    let mismatch_in_pattern = mapping_match_pattern.contains('x') || mapping_match_pattern.contains('X');
+    let mismatch_in_pattern = mapping_match_pattern.contains('x') ||
+                              mapping_match_pattern.contains('X');
 
     // first parse the fasta file
     let geneids = process_fasta(&fasta_file_arg, &fasta_re);
 
     // now parse the samfile
-    let (mapped_geneids, count_total) = process_sam(&sam_file_arg, mismatch_in_pattern, &mapping_match_pattern);
+    let (mapped_geneids, count_total) =
+        process_sam(&sam_file_arg, mismatch_in_pattern, &mapping_match_pattern);
 
     println!("sgRNA\tCount");
     for (k, v) in &mapped_geneids {
@@ -40,8 +46,12 @@ fn main() {
     println!("{}\t{}", count_total, count_total);
 }
 
-fn parse_args(fasta_file_arg: &mut String, sam_file_arg: &mut String, mapping_match_pattern: &mut String,
-geneid_pattern: &mut String, logfile_out: &mut String) {
+
+fn parse_args(fasta_file_arg: &mut String,
+              sam_file_arg: &mut String,
+              mapping_match_pattern: &mut String,
+              geneid_pattern: &mut String,
+              logfile_out: &mut String) {
     // put the argparsing in its own scope
     let mut cli_parser = ArgumentParser::new();
     cli_parser.set_description("mapper for CRISPRanalyzer");
@@ -55,34 +65,32 @@ geneid_pattern: &mut String, logfile_out: &mut String) {
         .required();
 
     cli_parser.refer(mapping_match_pattern)
-        .add_option(&["-m", "--match-pattern"], Store,
-                    "Mapping match pattern e.g. M{20,21}$");
+        .add_option(&["-m", "--match-pattern"], Store, "Mapping match pattern e.g. M{20,21}$");
 
-    cli_parser.refer(geneid_pattern).add_option(&["-g", "--geneid-pattern"],
-                                                     Store,
-                                                     "GeneId pattern to parse, e.g. '_'");
+    cli_parser.refer(geneid_pattern)
+        .add_option(&["-g", "--geneid-pattern"], Store, "GeneId pattern to parse, e.g. '_'");
 
-    cli_parser.refer(logfile_out).add_option(&["-l", "--logfile"],
-                                                  Store,
-                                                  "Logfile filename");
+    cli_parser.refer(logfile_out).add_option(&["-l", "--logfile"], Store, "Logfile filename");
 
     cli_parser.parse_args_or_exit();
 }
 
+
 fn process_fasta(fasta_file: &str, fasta_re: &Regex) -> HashSet<String> {
     let mut geneids = HashSet::<String>::new();
 
-    let fasta_file =
-        BufReader::new(File::open(fasta_file).expect("Problem opening fastq file"));
+    let fasta_file = BufReader::new(File::open(fasta_file).expect("Problem opening fastq file"));
 
     for line in fasta_file.lines() {
         let ln = line.expect("programmer error in reading fasta line by line");
 
         geneids.extend(
-            fasta_re.captures_iter(&ln).map(|captures: regex::Captures| // iterate over all Matches, which may have multiple capture groups each
-                                                captures.get(1) // of this match, take the first capture group
-                                                    .expect("fasta regex match should have had first capture group")
-                                                    .as_str().to_owned() // make Owned copy of capture-group contents
+            fasta_re.captures_iter(&ln)
+                // iterate over all Matches, which may have multiple capture groups each
+                .map(|captures: regex::Captures|
+                    captures.get(1) // of this match, take the first capture group
+                    .expect("fasta regex match should have had first capture group")
+                    .as_str().to_owned() // make Owned copy of capture-group contents
             )
         );
     }
@@ -91,10 +99,14 @@ fn process_fasta(fasta_file: &str, fasta_re: &Regex) -> HashSet<String> {
 }
 
 
-fn process_sam(sam_file: &str, mismatch_in_pattern: bool, mapping_match_pattern: &str) -> (HashMap<String, i32>, u32) {
+fn process_sam(sam_file: &str,
+               mismatch_in_pattern: bool,
+               mapping_match_pattern: &str)
+               -> (HashMap<String, i32>, u32) {
+
     // our buffer for the sam parser
-    let sam_file =
-        BufReader::new(File::open(sam_file).expect("Problem opening fastq file")).lines();
+    let sam_file = BufReader::new(File::open(sam_file).expect("Problem opening fastq file"))
+        .lines();
 
     let sam_mismatch_re =
         Regex::new(r"MD:Z:([0-9]+)([A-Z]+)[0-9]+").expect("programmer error in mismatch regex");
@@ -114,9 +126,8 @@ fn process_sam(sam_file: &str, mismatch_in_pattern: bool, mapping_match_pattern:
         }
 
         count_total += 1;
+
         // ----------the basic algorithm starts here ---
-
-
         // now split
         let al_arr: Vec<&str> = next_line.trim_right().split("\t").collect();
         //println!("{}", al_arr[2]);
@@ -136,9 +147,8 @@ fn process_sam(sam_file: &str, mismatch_in_pattern: bool, mapping_match_pattern:
             found_mismatch = true;
         }
 
-        // do some prechecks to safe computation time...skip the obvious
-        let skip = !mismatch_in_pattern && found_mismatch ||
-            mismatch_in_pattern && !found_mismatch;
+        // do some prechecks to save computation time...skip the obvious
+        let skip = !mismatch_in_pattern && found_mismatch || mismatch_in_pattern && !found_mismatch;
         if !skip {
             // build / expand cigar string, e.g. 20M -> MMMMMMMMMMMMMMMMMMMM, 10M,1I,5D ->
             // MMMMMMMMMMIDDDDD, 20M1D =
@@ -150,6 +160,7 @@ fn process_sam(sam_file: &str, mismatch_in_pattern: bool, mapping_match_pattern:
                     match_string.push_str(&caps[2]);
                 }
             }
+
             // now introduce mismatches int the string if needed
             if found_mismatch {
                 for pos in mm_positions {
@@ -157,6 +168,7 @@ fn process_sam(sam_file: &str, mismatch_in_pattern: bool, mapping_match_pattern:
                     match_string.insert_str(pos, "X");
                 }
             }
+
             // now apply input mapping regex
             if mapping_match_re.is_match(&match_string) {
                 let x = al_arr[2].to_owned().clone();
